bc583196363316c79c91fd87c4f0428d92410760
Igor <ipo@newpharma.ro>
2022-11-28 14:43:45 +0200

    Homework from module 6

---
+++ tema6c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+user = input("What is your user name? ")
+type(user)
+job = input("What is your current job? ")
+type(job)
+print("My user name is " + user +". I'm a " + job + ".")
